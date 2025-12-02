052e14105934e5d904a8d4ec9c4b19849d32a0b3
MrSallivan <nik.sallivan@gmail.com>
2025-12-02 18:46:28 +1000

    start function CreateTodoLayout

---
+++ ToDoApp/utils.js
@@ -0,0 +1,8 @@
+const TODOS_KEY = 'todos'
+
+export const saveTodosIntoLocalStorage = (todos) => {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos))
+}
+export const getTodosFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem(TODOS_KEY)) || []
+}
